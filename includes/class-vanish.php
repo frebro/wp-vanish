68cdcca6e0369692562aa5f7c9679a7d7d69aa25
Fredrik Broman <frebro@gmail.com>
2015-04-03 12:36:46 +0200

    version bump to 1.0.2

--- includes/class-vanish.php
+++ includes/class-vanish.php
@@ -70,7 +70,7 @@ class Vanish {
 
     $this->basename = $basename;
     $this->name = 'vanish';
-    $this->version = '1.0.1';
+    $this->version = '1.0.2';
 
     $this->load_dependencies();
     $this->set_locale();
